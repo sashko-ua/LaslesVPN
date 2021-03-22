d10b35195bf5445e7561b78b4a4f7559dc2fd8c7
Sashko <sashkoace@gmail.com>
2021-03-22 21:24:19 +0200

    completed the layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+import { tns } from "tiny-slider/src/tiny-slider";
+
+const slider = tns({
+    container: '.customers__slider',
+    items: 3,
+    slideBy: 1,
+    controlsText: [
+        '<img src="../img/arrow-back-fill.svg" alt="arrow-back">',
+        '<img src="../img/arrow-next-fill.svg" alt="arrow-next">'
+    ],
+    navAsThumbnails: true,
+    autoplay: true,
+    autoplayButtonOutput: false
+});
